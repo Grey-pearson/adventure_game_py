18f37a945589af7ce7b91e8a6145a769b35ef422
grey <greytpearson@gmail.com>
2024-06-14 12:28:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 # file = "2D Pixel Dungeon Asset Pack/character and tileset/Dungeon_Tileset.png"
 
 # class Game:
@@ -14,22 +15,8 @@ y = 40
 
 width = 40
 # come up with a different way to create random stats, using random ig, 
-# height = [10,20,30,40,50,60,70,80,90,100,110,120,130,140]
-height = [ 20,
- 10,
- 30,
-140,
- 80,
- 70,
- 90,
-110,
-100,
-130,
-150,
- 50,
- 60,
- 40,
-120,]
+height = [10,20,30,40,50,60,70,80,90,100,110,120,130,140]
+# height = [ 20, 10, 30,140, 80, 70, 90,110,100,130,150, 50, 60,40,120,160]
 
 # whats this do?
 run = True
@@ -46,6 +33,7 @@ while run:
         if event.type == pg.QUIT:
             run=False
     if keys[pg.K_SPACE]:
+        random.shuffle(height)
         execute = True
     if execute == False:
         win.fill((0,0,0))
@@ -63,5 +51,5 @@ while run:
                 show(height)
                 pg.time.delay(50)
                 pg.display.update()
-                
+pg.display.flip()       
 pg.quit()
