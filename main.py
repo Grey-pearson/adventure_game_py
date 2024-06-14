2a22a4855cee3419c64cbbdbd3535b5021396c24
grey <greytpearson@gmail.com>
2024-06-14 09:56:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import os
 import sys
 import algorithms 
 
+pg.init()
+
 # file = "2D Pixel Dungeon Asset Pack/character and tileset/Dungeon_Tileset.png"
 
 # class Game:
 #     W = 160
 #     H = 160
 #     SIZE = W, H
+
