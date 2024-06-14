f4e708b7bc4ce1e04d5523726b4a69368a0b35f7
grey <greytpearson@gmail.com>
2024-06-14 12:25:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,23 @@ y = 40
 
 width = 40
 # come up with a different way to create random stats, using random ig, 
-height = [10,20,30,40,50,60,70,80,90,100,110,120,130,140]
+# height = [10,20,30,40,50,60,70,80,90,100,110,120,130,140]
+height = [ 20,
+ 10,
+ 30,
+140,
+ 80,
+ 70,
+ 90,
+110,
+100,
+130,
+150,
+ 50,
+ 60,
+ 40,
+120,]
+
 # whats this do?
 run = True
 
@@ -46,6 +62,6 @@ while run:
                 win.fill((0,0,0))
                 show(height)
                 pg.time.delay(50)
-                pg.dispaly.udpate()
+                pg.display.udpate()
                 
 pg.quit()
