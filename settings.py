0869c319d1d0f93c55bd288f432d3493fc25c21a
grey <greytpearson@gmail.com>
2024-06-19 15:33:26 -0600

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,18 @@
+TITLE = "Adventure Game"
+ICON = "2D Pixel Dungeon Asset Pack/Character_animation/monsters_idle/skull/v2/skull_v2_3.png"
+
+WIDTH = 1280
+HEIGHT = 720
+
+FPS = 60
+
+# grid
+TILESIZE = 16
+GRIDWIDTH = WIDTH/TILESIZE
+GRIDHEIGHT = HEIGHT/TILESIZE
+
+# colors
+TAN = (234, 219, 198)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
