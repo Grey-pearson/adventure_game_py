0f382add553e358994fcc2b2b2da9e41bfd0e4ff
grey <greytpearson@gmail.com>
2024-06-19 15:37:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ surfObj = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
 
 class Map:
-    
+    def __init__(self, filename):
+        self.data = []
+        with open(filename, 'rt') as f:
+            for line in f:
+                self.data.append(line)
